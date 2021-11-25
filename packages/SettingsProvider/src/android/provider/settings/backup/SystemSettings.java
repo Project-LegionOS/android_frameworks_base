f8b7e03055c24187b9f8c4da459791eb5937e1ee
jhonboy121 <alfredmathew05@gmail.com>
2021-11-25 17:32:41 +0530

    base: allow hiding navbar [1/3]
    Change-Id: Iba3b1a3b7ee56d87564f06c5b5fd084c1de5edc1
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Signed-off-by: rajkale99 <kaleraj.rk@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -91,5 +91,6 @@ public class SystemSettings {
         Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD,
         Settings.System.ENABLE_RIPPLE_EFFECT,
         Settings.System.LOCKSCREEN_BATTERY_INFO,
+        Settings.System.FULLSCREEN_GESTURES,
     };
 }
