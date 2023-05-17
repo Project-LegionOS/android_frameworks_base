Colt-Enigma/platform_frameworks_base

base: Fod -> Udfps
Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

core/java/com/android/internal/util/xtended/udfps/UdfpsUtils.java
@@ -13,14 +13,14 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-package com.android.internal.util.xtended.fod;
+package com.android.internal.util.xtended.udfps;
 
 import android.content.Context;
 
 import com.android.internal.util.ArrayUtils;
 
-public class FodUtils {
-    public static boolean hasFodSupport(Context context) {
+public class UdfpsUtils {
+    public static boolean hasUdfpsSupport(Context context) {
         int[] udfpsProps = context.getResources().getIntArray(
                 com.android.internal.R.array.config_udfps_sensor_props);
 

packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -102,7 +102,7 @@ import com.android.internal.policy.IKeyguardExitCallback;
 import com.android.internal.policy.IKeyguardStateCallback;
 import com.android.internal.policy.ScreenDecorationsUtils;
 import com.android.internal.util.LatencyTracker;
-import com.android.internal.util.xtended.fod.FodUtils;
+import com.android.internal.util.xtended.udfps.UdfpsUtils;
 import com.android.internal.widget.LockPatternUtils;
 import com.android.keyguard.KeyguardConstants;
 import com.android.keyguard.KeyguardDisplayManager;

Project-LegionOS/android_frameworks_base

Signed-off-by: rajkale99 <kaleraj.rk@gmail.com>

core/java/com/android/internal/util/legion/udfps/UdfpsUtils.java
@@ -13,14 +13,14 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-package com.android.internal.util.legion.fod;
+package com.android.internal.util.legion.udfps;
 
 import android.content.Context;
 
 import com.android.internal.util.ArrayUtils;
 
-public class FodUtils {
-    public static boolean hasFodSupport(Context context) {
+public class UdfpsUtils {
+    public static boolean hasUdfpsSupport(Context context) {
         int[] udfpsProps = context.getResources().getIntArray(
                 com.android.internal.R.array.config_udfps_sensor_props);
 

packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -101,7 +101,7 @@ import com.android.internal.policy.IKeyguardExitCallback;
 import com.android.internal.policy.IKeyguardStateCallback;
 import com.android.internal.policy.ScreenDecorationsUtils;
 import com.android.internal.util.LatencyTracker;
-import com.android.internal.util.legion.fod.FodUtils;
+import com.android.internal.util.legion.udfps.UdfpsUtils;
 import com.android.internal.widget.LockPatternUtils;
 import com.android.keyguard.KeyguardConstants;
 import com.android.keyguard.KeyguardDisplayManager;