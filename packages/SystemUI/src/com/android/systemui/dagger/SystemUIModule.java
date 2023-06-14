c83e4392a06a3dea771c299cff54ce0972741cfd
Michael Bestas <mkbestas@gmail.com>
2023-06-14 11:52:56 +0300

    fixup! SystemUI: Add caffeine QS tile
    Change-Id: I16eab8d0ec496bbac7e32eea370c846573349cc9
    Signed-off-by: rajkale99 <kaleraj.rk@gmail.com>

--- packages/SystemUI/src/com/android/systemui/dagger/SystemUIModule.java
+++ packages/SystemUI/src/com/android/systemui/dagger/SystemUIModule.java
@@ -49,6 +49,7 @@ import com.android.systemui.flags.FlagsModule;
 import com.android.systemui.fragments.FragmentService;
 import com.android.systemui.keyboard.KeyboardModule;
 import com.android.systemui.keyguard.data.BouncerViewModule;
+import com.android.systemui.legion.LegionModule;
 import com.android.systemui.log.dagger.LogModule;
 import com.android.systemui.mediaprojection.appselector.MediaProjectionModule;
 import com.android.systemui.model.SysUiState;
@@ -156,6 +157,7 @@ import dagger.Provides;
             FooterActionsModule.class,
             GarbageMonitorModule.class,
             KeyboardModule.class,
+            LegionModule.class,
             LogModule.class,
             MediaProjectionModule.class,
             MotionToolModule.class,
