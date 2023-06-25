eaa8ba2098aa8382c2cf1a70801037a05aec823d
rajkale99 <kaleraj.rk@gmail.com>
2023-06-25 13:56:31 +0530

    Revert "SystemUI: Avoid unnecessary keyguard indication controller recreation"
    This reverts commit c1019f934fa0177605e4800890fa7f4434438c09.
    
    Signed-off-by: rajkale99 <kaleraj.rk@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -329,12 +329,10 @@ public class KeyguardIndicationController {
             R.id.keyguard_indication_text_bottom);
         mInitialTextColorState = mTopIndicationView != null
                 ? mTopIndicationView.getTextColors() : ColorStateList.valueOf(Color.WHITE);
-        if (mRotateTextViewController == null || !mRotateTextViewController.isAttachedToWindow()) {
-            mRotateTextViewController = new KeyguardIndicationRotateTextViewController(
-                mLockScreenIndicationView,
-                mExecutor,
-                mStatusBarStateController);
-        }
+        mRotateTextViewController = new KeyguardIndicationRotateTextViewController(
+            mLockScreenIndicationView,
+            mExecutor,
+            mStatusBarStateController);
         updateDeviceEntryIndication(false /* animate */);
         updateOrganizedOwnedDevice();
         if (mBroadcastReceiver == null) {
