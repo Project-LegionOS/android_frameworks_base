f8f9d91529951d5a3579c77fe0e016b2297533d1
rajkale99 <kaleraj.rk@gmail.com>
2023-06-29 18:39:51 +0530

    base: revert several commits
    Revert "base: Allow to skip confirmation in biometric auth dialog [1/2]"
    
    This reverts commit f9889242a6a78d2699f75e2d969f2777e0c9a817.
    
    Signed-off-by: rajkale99 <kaleraj.rk@gmail.com>
    
    Revert "Fix for NullPointerException in SmsMessage"
    
    This reverts commit abab6856bf8bdb160e5080b1eceb7eb143b94962.
    
    Signed-off-by: rajkale99 <kaleraj.rk@gmail.com>
    
    Revert "Fix potential crash when exit splash screen"
    
    This reverts commit a12f36473095ff9b468887c6476e66bba137cef8.
    
    Signed-off-by: rajkale99 <kaleraj.rk@gmail.com>
    
    Revert "SystemUI: allow set combined QS with config"
    
    This reverts commit 8c9e920832bd03dc75347146496576ba1b6389ea.
    
    Signed-off-by: rajkale99 <kaleraj.rk@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -223,7 +223,6 @@ public class SecureSettings {
         Settings.Secure.BLUETOOTH_LE_BROADCAST_CODE,
         Settings.Secure.BLUETOOTH_LE_BROADCAST_APP_SOURCE_NAME,
         Settings.Secure.TETHERING_ALLOW_VPN_UPSTREAMS,
-        Settings.Secure.IGNORE_AUTH_CONFIRMATION,
         Settings.Secure.GESTURE_NAVBAR_LENGTH_MODE,
         Settings.Secure.LOCK_SCREEN_WEATHER_ENABLED,
         Settings.Secure.ACCESSIBILITY_FONT_SCALING_HAS_BEEN_CHANGED
