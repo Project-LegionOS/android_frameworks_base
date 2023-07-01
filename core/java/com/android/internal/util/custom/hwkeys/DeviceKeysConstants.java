4ea14a48632730c8d260089ae6fa14d82538f03a
rajkale99 <kaleraj.rk@gmail.com>
2023-07-01 11:11:30 +0530

    Revert "DeviceKeysConstants: Remove split screen actions"
    This reverts commit ddeb7e79dec38b2c8df13bad3e222e56dfaef481.

--- core/java/com/android/internal/util/custom/hwkeys/DeviceKeysConstants.java
+++ core/java/com/android/internal/util/custom/hwkeys/DeviceKeysConstants.java
@@ -33,6 +33,7 @@ public class DeviceKeysConstants {
         VOICE_SEARCH,
         LAUNCH_CAMERA,
         SLEEP,
+        SPLIT_SCREEN,
         SCREENSHOT;
 
         public static Action fromIntSafe(int id) {
